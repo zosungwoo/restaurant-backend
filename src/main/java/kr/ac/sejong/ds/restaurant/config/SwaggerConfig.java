c6bb17273830a12ec93ef8b08025ab3b64025ba7
Sungwoo Cho <seouk77@gmail.com>
2024-02-02 15:42:42 +0900

    Add API Doc using Swagger

---
+++ src/main/java/kr/ac/sejong/ds/restaurant/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package kr.ac.sejong.ds.restaurant.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "Restaurant Recommendation API",
+                description = "식당 추천 서비스 Restaurant Recommendation API 문서",
+                version = "v1"))
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi chatOpenApi() {
+        String[] paths = {"/v1/**"};
+
+        return GroupedOpenApi.builder()
+                .group("Restaurant Recommendation API")
+                .pathsToMatch(paths)
+                .build();
+    }
+}
