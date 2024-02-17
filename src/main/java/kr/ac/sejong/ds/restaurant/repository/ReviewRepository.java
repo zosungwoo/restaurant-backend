bf41e407a19c9094635e0ea04ce29e6513fb7661
Sungwoo Cho <seouk77@gmail.com>
2024-02-18 01:01:34 +0900

    [Fix] Use fetch join for review & user response (LAZY loading)

--- src/main/java/kr/ac/sejong/ds/restaurant/repository/ReviewRepository.java
+++ src/main/java/kr/ac/sejong/ds/restaurant/repository/ReviewRepository.java
@@ -3,11 +3,13 @@ package kr.ac.sejong.ds.restaurant.repository;
 import kr.ac.sejong.ds.restaurant.domain.Restaurant;
 import kr.ac.sejong.ds.restaurant.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    @Query("select r from Review r join fetch r.user")
     List<Review> findAllByRestaurantId(Long restaurantId);
 }
