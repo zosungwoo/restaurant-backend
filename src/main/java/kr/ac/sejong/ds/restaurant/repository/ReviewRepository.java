1487fd0ac5e741e42e589397873f9d51cc250c16
Sungwoo Cho <seouk77@gmail.com>
2024-02-18 22:22:15 +0900

    [Fix] Fetch join with 'where'

--- src/main/java/kr/ac/sejong/ds/restaurant/repository/ReviewRepository.java
+++ src/main/java/kr/ac/sejong/ds/restaurant/repository/ReviewRepository.java
@@ -4,12 +4,13 @@ import kr.ac.sejong.ds.restaurant.domain.Restaurant;
 import kr.ac.sejong.ds.restaurant.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    @Query("select r from Review r join fetch r.user")
-    List<Review> findAllByRestaurantId(Long restaurantId);
+    @Query("select r from Review r join fetch r.user where r.restaurant.id = :restaurantId")
+    List<Review> findAllByRestaurantId(@Param("restaurantId") Long restaurantId);
 }
