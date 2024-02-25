f8f04bb501bcfecb576c8022fc7b33c784443397
Sungwoo Cho <seouk77@gmail.com>
2024-02-26 02:06:54 +0900

    Modify swagger api docs

--- src/main/java/kr/ac/sejong/ds/restaurant/controller/RestaurantController.java
+++ src/main/java/kr/ac/sejong/ds/restaurant/controller/RestaurantController.java
@@ -1,5 +1,7 @@
 package kr.ac.sejong.ds.restaurant.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.ac.sejong.ds.restaurant.dto.RestaurantResponseDto;
 import kr.ac.sejong.ds.restaurant.service.RestaurantService;
 import lombok.RequiredArgsConstructor;
@@ -9,11 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Tag(name = "레스토랑")
 @RestController
 @RequiredArgsConstructor
 public class RestaurantController {
     private final RestaurantService restaurantService;
 
+    @Operation(summary = "모든 레스토랑 조회")
     @GetMapping("/api/restaurants")
     public List<RestaurantResponseDto> getAllRestaurant() {
         List<RestaurantResponseDto> restaurantResponseDto = restaurantService.findRestaurants()
--- src/main/java/kr/ac/sejong/ds/restaurant/controller/ReviewController.java
+++ src/main/java/kr/ac/sejong/ds/restaurant/controller/ReviewController.java
@@ -1,5 +1,8 @@
 package kr.ac.sejong.ds.restaurant.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.ac.sejong.ds.restaurant.domain.Review;
 import kr.ac.sejong.ds.restaurant.dto.ReviewResponseDto;
 import kr.ac.sejong.ds.restaurant.service.ReviewService;
@@ -11,13 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Tag(name = "리뷰")
 @RestController
 @RequiredArgsConstructor
 public class ReviewController {
     private final ReviewService reviewService;
 
+    @Operation(summary = "레스토랑 id에 따른 리뷰 조회")
     @GetMapping("/api/restaurants/{restaurantId}/reviews")
-    public List<ReviewResponseDto> getAllReviewByRestaurant(@PathVariable("restaurantId") Long restaurantId) {
+    public List<ReviewResponseDto> getAllReviewByRestaurant(@Parameter(description = "레스토랑 id") @PathVariable("restaurantId") Long restaurantId) {
         List<ReviewResponseDto> reviewResponseDto = reviewService.findReviewByRestaurant(restaurantId)
                 .stream().map(ReviewResponseDto::new).collect(Collectors.toList());
         return reviewResponseDto;
